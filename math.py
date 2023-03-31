6f2029304b0fb5179b6715f4c4faf21590173261
student <kevinenzayanga@gmail.com>
2023-03-31 15:23:56 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def addition (a,b):
+    answer = a+b
+    return answer
+
+def subtract (a,b):
+    sub = a-b
+    return sub
+
+def divide (a,b):
+    div = a/b
+    return div
+
+def multiply (a,b):
+    multi = a*b
+    return multi
+
+def remainder (a,b):
+    rem = a%b
+    return rem
